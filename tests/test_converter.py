9db86f1384c73dee5c5b83669d2bd79ce02e5218
Jon Bos <bos.jon@gmail.com>
2020-03-24 10:49:20 -0500

    test: some additional test cases...I can't believe this works.

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -7,19 +7,23 @@ from src.converter import number_to_numeral
     (1, "I"),
     (2, "II"),
     (3, "III"),
+    (4, "IV"),
+    (5, "V"),
+    (6, "VI"),
+    (7, "VII"),
+    (8, "VIII"),
+    (9, "IX"),
     (10, "X"),
     (20, "XX"),
     (30, "XXX"),
+    (40, "XL"),
     (100, "C"),
     (200, "CC"),
     (300, "CCC"),
+    (400, "CD"),
     (1000, "M"),
     (2000, "MM"),
     (3000, "MMM"),
-    (5, "V"),
-    (4, "IV")
 ])
 def test_should_convert_number_to_numeral(number, expected_numeral):
     assert number_to_numeral(number) == expected_numeral
-
-
