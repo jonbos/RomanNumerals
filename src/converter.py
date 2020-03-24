58577792a589dacccfd14b69d48d1d5a7ca1ae36
Jon Bos <bos.jon@gmail.com>
2020-03-24 09:00:18 -0500

    test: convert 10,20,30 returns numeral

--- src/converter.py
+++ src/converter.py
@@ -1,2 +1,20 @@
+numerals = {1: "I", 10: "X"}
+
+
+# mapping={
+#     1:[1],
+#     2:[1,1],
+#     3: [1,1,1],
+#     4: [1,5],
+#     5: [5],
+#     6: [5,1],
+#     7: [5,1,1],
+#     8: [5,1,1,1],
+#     9: [1,10],
+#     10: [10]
+# }
 def number_to_numeral(number):
-    return "I" * number;
+    if (number < 10):
+        return numerals[1] * number;
+    else:
+        return numerals[10] * (number // 10)
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -5,7 +5,11 @@ from src.converter import number_to_numeral
 
 @pytest.mark.parametrize("number,expected_numeral", [
     (1, "I"),
-    (2, "II")
+    (2, "II"),
+    (3, "III"),
+    (10, "X"),
+    (20, "XX"),
+    (30, "XXX")
 ])
 def test_should_convert_number_to_numeral(number, expected_numeral):
     assert number_to_numeral(number) == expected_numeral
