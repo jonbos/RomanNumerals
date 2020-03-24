0e545a496c04d935bfa1d95055f4bfcff340d81c
Jon Bos <bos.jon@gmail.com>
2020-03-24 11:41:09 -0500

    test: split_on_decimal_place returns array of decimal places

--- src/number_util.py
+++ src/number_util.py
@@ -1,2 +1,7 @@
-def split_on_decimal_place(number):
-    return [number]
+def split_int_into_factors_of_ten(number):
+    number_digits = [int(digit) for digit in str(number)]
+    digits_and_powers_of_ten = zip(number_digits, range(len(number_digits) - 1, -1, -1))
+    res = []
+    for digit_tuple in digits_and_powers_of_ten:
+        res.append(digit_tuple[0] * (10 ** digit_tuple[1]))
+    return res
--- tests/test_number_util.py
+++ tests/test_number_util.py
@@ -1,10 +1,13 @@
 import pytest
 
-from src.number_util import split_on_decimal_place
+from src.number_util import split_int_into_factors_of_ten
 
 
 @pytest.mark.parametrize("number,expected_array", [
     (1, [1]),
+    (10, [10, 0]),
+    (11, [10, 1]),
+    (499, [400, 90, 9]),
 ])
-def test_should_convert_number_to_numeral(number, expected_array):
-    assert split_on_decimal_place(number) == expected_array
+def test_should_split_number_on_decimal_place(number, expected_array):
+    assert split_int_into_factors_of_ten(number) == expected_array
